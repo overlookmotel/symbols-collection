857fa2b355cb2a8b9d76bded32017eaacbcb3084
overlookmotel <theoverlookmotel@gmail.com>
2019-12-26 17:53:53 +0000

    Tests: Tests for invalid input errors

--- test/index.test.js
+++ test/index.test.js
@@ -33,3 +33,39 @@ it('symbols named with namespace', () => {
 	const symbols = symbolsCollection('foo', ['BAR']);
 	expect(String(symbols.BAR)).toBe('Symbol(foo.BAR)');
 });
+
+describe('throws error if', () => {
+	describe('namespace', () => {
+		it('not provided', () => {
+			expect(() => {
+				symbolsCollection(null, ['BAR']);
+			}).toThrowWithMessage(TypeError, 'namespace must be a string');
+		});
+
+		it('is not a string', () => {
+			expect(() => {
+				symbolsCollection(123, ['BAR']);
+			}).toThrowWithMessage(TypeError, 'namespace must be a string');
+		});
+	});
+
+	describe('names', () => {
+		it('not provided', () => {
+			expect(() => {
+				symbolsCollection('foo');
+			}).toThrowWithMessage(TypeError, 'names must be an array of strings');
+		});
+
+		it('not an array', () => {
+			expect(() => {
+				symbolsCollection('foo', 123);
+			}).toThrowWithMessage(TypeError, 'names must be an array of strings');
+		});
+
+		it('contains a non-string', () => {
+			expect(() => {
+				symbolsCollection('foo', ['BAR', 123]);
+			}).toThrowWithMessage(TypeError, 'names must be an array of strings');
+		});
+	});
+});
